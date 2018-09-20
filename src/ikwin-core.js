c834e2ff55e3186e5c5d38a6b830114656462478
Alexander Korzh <appmux@gmail.com>
2018-09-20 08:00:38 -0400

    Fixed indentation.

--- src/ikwin-core.js
+++ src/ikwin-core.js
@@ -4,29 +4,29 @@ import ServiceManager from 'ikwin-service-manager'
 window.ikwin = window.ikwin || {ready: false}
 
 function getServiceManager() {
-    if (!window.ikwin.serviceManager) {
-      window.ikwin.serviceManager = new ServiceManager()
-    }
+  if (!window.ikwin.serviceManager) {
+    window.ikwin.serviceManager = new ServiceManager()
+  }
 
-    return window.ikwin.serviceManager
+  return window.ikwin.serviceManager
 }
 
 function init() {
-    window.ikwin.ready = true
+  window.ikwin.ready = true
 }
 
 function ready() {
-    if (window.ikwin.ready) {
-        return Promise.resolve(window.ikwin.serviceManager)
-    }
+  if (window.ikwin.ready) {
+    return Promise.resolve(window.ikwin.serviceManager)
+  }
 
-    return new Promise((resolve, reject) => {
-        expect(() => window.ikwin.ready).then(() => resolve(window.ikwin.serviceManager))
-    })
+  return new Promise((resolve, reject) => {
+    expect(() => window.ikwin.ready).then(() => resolve(window.ikwin.serviceManager))
+  })
 }
 
 export default {
-    getServiceManager,
-    init,
-    ready
+  getServiceManager,
+  init,
+  ready
 }
